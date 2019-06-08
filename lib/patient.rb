31cd0f1c69e602cac57dc1d03ddcfb34536ed34e
Warren Yao <warrenyca905@gmail.com>
2019-06-08 23:00:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,14 +1,17 @@
 class Patient
   attr_accessor :name
+  attr_reader :appointments
   
   @@all = []
   
   def initialize(name)
     @name = name
+    @appointments = []
   end
   
   def new_appointment(doctor, date)
     n_a = Appointment.new(date, self, doctor)
+    appointments << n_a
     n_a
   end
 end
