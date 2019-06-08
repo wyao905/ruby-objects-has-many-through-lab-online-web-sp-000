2e2bf5995ee81c5177a3cadbc19f7363d15a2534
Warren Yao <warrenyca905@gmail.com>
2019-06-08 22:41:29 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,15 @@
 class Appointment
   attr_accessor :date, :patient, :doctor
   
+  @@all = []
+  
   def initialize(date, patient, doctor)
     @date = date
     @patient = patient
     @doctor = doctor
+  end
+  
+  def self.all
+    @@all
   end
 end
