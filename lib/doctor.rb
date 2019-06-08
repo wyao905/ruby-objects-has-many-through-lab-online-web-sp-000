a0eb595cbd418901ff0809dbdc68ee97bf504fac
Warren Yao <warrenyca905@gmail.com>
2019-06-08 22:31:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,10 +1,11 @@
 class Doctor
-  attr_accessor :name
+  attr_accessor :name, :appointments
   
   @@all = []
   
   def initialize(name)
     @name = name
+    @appointments = []
     Doctor.all << self
   end
   
@@ -14,7 +15,7 @@ class Doctor
   end
   
   def new_appointment(patient, date)
-    Appointment.new(date, patient, self)
+    appointments << Appointment.new(date, patient, self)
   end
   
   def self.all
