3ab864fc3c924959b603dc9d9f61924f6efd7304
Warren Yao <warrenyca905@gmail.com>
2019-06-08 19:24:54 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+  attr_reader :songs, :all_genres
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @all_genres = []
+    @@all << self
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    songs << song
+    song
+  end
+  
+  def genres
+    songs.each {|song| @all_genres << song.genre if !@all_genres.include?(song.genre)}
+    all_genres
+  end
+  
+  def self.all
+    @@all
+  end
+end
